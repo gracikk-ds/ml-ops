01eee2f1291f9b475bf9e5ae7f9348732659c37f
gordeev.al <aleksandr_gordeev2@epam.com>
2022-06-09 14:28:37 +0300

    mlflow experimnt

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,72 +5,72 @@ import yaml
 import json
 import click
 import mlflow
-import pickle
 import optuna
 import logging
 import pandas as pd
 from pathlib import Path
-from dotenv import load_dotenv
-from datetime import datetime
 from functools import partial
+from dotenv import load_dotenv
 from matplotlib import pyplot as plt
+from mlflow.models import infer_signature
+from mlflow.tracking.client import MlflowClient
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 
 
-
 load_dotenv()
-
-ROOT = Path(__file__).parent.parent.parent
-
 remote_server_uri = os.getenv("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(remote_server_uri)
 
-current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-mlflow.set_experiment("linear_model_run_at_" + current_time)
+ROOT = Path(__file__).parent.parent.parent
 
 
 def objective(trial, params, x_train, y_train):
     """Run optuna trials"""
-    penalty = trial.suggest_categorical("penalty", params["penalty"])
-    c = trial.suggest_float(
-        "C",
-        float(params["C"]["upper_bound"]),
-        float(params["C"]["lower_bound"]),
-        log=True,
-    )
+    with mlflow.start_run(nested=True):
+        penalty = trial.suggest_categorical("penalty", params["penalty"])
+        c = trial.suggest_float(
+            "C",
+            float(params["C"]["upper_bound"]),
+            float(params["C"]["lower_bound"]),
+            log=True,
+        )
 
-    clf = LogisticRegression(solver="liblinear", penalty=penalty, C=c)
+        clf = LogisticRegression(solver="liblinear", penalty=penalty, C=c)
 
-    target = cross_val_score(
-        clf, x_train, y_train, n_jobs=-1, cv=3, scoring="precision"
-    ).mean()
+        mlflow.log_params({"C": c, "penalty": penalty})
+
+        target = cross_val_score(
+            clf, x_train, y_train, n_jobs=-1, cv=3, scoring="precision"
+        ).mean()
+
+        mlflow.log_metrics({"train_precision": target})
 
     return target
 
 
 @click.command()
 @click.option("--path_to_dataset", default="data/processed/train.csv", type=str)
-@click.option("--path_to_model_storage", default="models", type=str)
-@click.option("--path_to_metrics_storage", default="reports/metrics", type=str)
-def main(path_to_dataset, path_to_model_storage, path_to_metrics_storage):
-    with mlflow.start_run():
+def main(path_to_dataset):
+    mlflow.set_experiment("linear_model_experiments_1")
+
+    with mlflow.start_run() as run:
         """Runs training job and save best model to model's storage"""
+
+        experiment_id = run.info.experiment_id
 
         logger = logging.getLogger(__name__)
         logger.info("Start model training process")
 
         path_to_dataset = ROOT / Path(path_to_dataset)
-        path_to_model_storage = ROOT / Path(path_to_model_storage)
-        path_to_metrics_storage = ROOT / Path(path_to_metrics_storage)
 
         # read dataset
-        train = pd.read_csv(path_to_dataset)
-
-        mlflow.log_text("it's a great run, ty!", "text.txt")
+        train = pd.read_csv(path_to_dataset).drop(columns=["Unnamed: 0"])
 
         # Now separate the dataset as response variable and feature variables
         x_train = train.drop("target", axis=1)
+
+        print(x_train.head())
         y_train = train["target"]
 
         # save best params
@@ -82,13 +82,53 @@ def main(path_to_dataset, path_to_model_storage, path_to_metrics_storage):
         study = optuna.create_study(direction="maximize")
         study.optimize(obj_partial, n_trials=200)
 
-        trial = study.best_trial
+        # find the best run, log its metrics as the final metrics of this run.
+        client = MlflowClient()
+        runs = client.search_runs(
+            [experiment_id],
+            "tags.mlflow.parentRunId = '{run_id}' ".format(run_id=run.info.run_id)
+        )
 
-        print("Best hyperparameters: {}".format(trial.params))
+        best_metric = 0
+        best_run = None
+        for r in runs:
+            if r.data.metrics["train_precision"] > best_metric:
+                if best_run is not None:
+                    run_id = best_run.info.run_id
+                    mlflow.delete_run(run_id)
+                best_run = r
+                best_metric = r.data.metrics["train_precision"]
+            else:
+                run_id = r.info.run_id
+                mlflow.delete_run(run_id)
+        mlflow.set_tag("params_search_best_run", best_run.info.run_id)
+
+        mlflow.log_metrics(
+            {
+                "train_precision": best_metric,
+            }
+        )
+
+        mlflow.log_params(
+            {
+                "C": best_run.data.params["C"],
+                "penalty": best_run.data.params["penalty"]
+            }
+        )
 
         # Let's run SVC again with the best parameters.
-        clf = LogisticRegression(solver="liblinear", **trial.params)
+        clf = LogisticRegression(solver="liblinear", **study.best_trial.params)
         clf.fit(x_train, y_train)
+
+        signature = infer_signature(x_train, y_train)
+
+        # Log the sklearn model and register as version 1
+        mlflow.sklearn.log_model(
+            sk_model=clf,
+            artifact_path="sklearn-model",
+            registered_model_name="sk-learn-log-reg",
+            signature=signature,
+        )
 
         explainer = shap.Explainer(clf.predict, x_train)
         shap_values = explainer(x_train)
@@ -101,12 +141,6 @@ def main(path_to_dataset, path_to_model_storage, path_to_metrics_storage):
             dpi=150,
             bbox_inches="tight",
         )
-
-        with open(str(path_to_metrics_storage / "hyper_params.json"), "w") as handler:
-            json.dump(trial.params, handler)
-
-        with open(str(path_to_model_storage / "finalized_model.pkl"), "wb") as handle:
-            pickle.dump(clf, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
         logger.info("done!")
 
--- src/models/validate_model.py
+++ src/models/validate_model.py
@@ -1,66 +1,94 @@
 # -*- coding: utf-8 -*-
+import os
 import json
 import click
-import pickle
+import mlflow
 import logging
 import pandas as pd
 from pathlib import Path
+from dotenv import load_dotenv
+from mlflow.tracking.client import MlflowClient
 from sklearn.metrics import precision_score, recall_score, roc_auc_score, roc_curve
 
+
+load_dotenv()
+remote_server_uri = os.getenv("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(remote_server_uri)
 
 ROOT = Path(__file__).parent.parent.parent
 
 
 @click.command()
 @click.option("--path_to_dataset", default="data/processed/test.csv", type=str)
-@click.option("--path_to_model_pkl", default="models/finalized_model.pkl", type=str)
 @click.option("--path_to_metrics_storage", default="reports/metrics", type=str)
-def main(path_to_dataset, path_to_model_pkl, path_to_metrics_storage):
+@click.option("--experiment_name", default=None)
+def main(path_to_dataset, path_to_metrics_storage, experiment_name):
     """Runs validation method"""
+    client = MlflowClient()
 
-    logger = logging.getLogger(__name__)
-    logger.info("Start predicting process")
+    if experiment_name is None:
 
-    path_to_dataset = ROOT / Path(path_to_dataset)
-    path_to_model_pkl = ROOT / Path(path_to_model_pkl)
-    path_to_metrics_storage = ROOT / Path(path_to_metrics_storage)
+        experiments = client.list_experiments()
+        current_experiment = experiments[-1]
+        df = mlflow.search_runs([current_experiment.experiment_id])
+        run_id = df.run_id.values[-1]
 
-    # read dataset
-    test = pd.read_csv(path_to_dataset)
+    else:
+        current_experiment = mlflow.get_experiment_by_name(experiment_name)
+        df = mlflow.search_runs([current_experiment.experiment_id])
+        run_id = df.run_id.values[-1]
 
-    # Now separate the dataset as response variable and feature variables
-    x_test = test.drop("target", axis=1)
-    y_test = test["target"]
+    with mlflow.start_run(run_id=run_id):
+        logger = logging.getLogger(__name__)
+        logger.info("Start predicting process")
 
-    with open(path_to_model_pkl, "rb") as f:
-        clf = pickle.load(f)
+        path_to_dataset = ROOT / Path(path_to_dataset)
+        path_to_metrics_storage = ROOT / Path(path_to_metrics_storage)
 
-    predictions = clf.predict(x_test)
-    predictions_proba = clf.predict_proba(x_test)
+        # read dataset
+        test = pd.read_csv(path_to_dataset).drop(columns=["Unnamed: 0"])
 
-    # Let's see how our model performed
-    precision = precision_score(y_test.values, predictions)
-    recall = recall_score(y_test.values, predictions)
-    roc_auc = roc_auc_score(y_test.values, predictions_proba[:, 1])
-    fpr, tpr, _ = roc_curve(y_test.values, predictions_proba[:, 1])
+        # Now separate the dataset as response variable and feature variables
+        x_test = test.drop("target", axis=1)
+        y_test = test["target"]
 
-    metrics = {
-        "train": {
-            "precision": precision,
-            "recall": recall,
-            "roc_auc": roc_auc,
+        latest_version = client.get_latest_versions(
+            "sk-learn-log-reg", stages=["None"]
+        )[0].version
+
+        clf = mlflow.sklearn.load_model(f"models:/sk-learn-log-reg/{latest_version}")
+
+        predictions = clf.predict(x_test)
+        predictions_proba = clf.predict_proba(x_test)
+
+        # Let's see how our model performed
+        precision = precision_score(y_test.values, predictions)
+        recall = recall_score(y_test.values, predictions)
+        roc_auc = roc_auc_score(y_test.values, predictions_proba[:, 1])
+
+        mlflow.log_metrics({"test_precision": precision})
+        mlflow.log_metrics({"test_recall": recall})
+        mlflow.log_metrics({"test_roc_auc": roc_auc})
+
+        fpr, tpr, _ = roc_curve(y_test.values, predictions_proba[:, 1])
+
+        metrics = {
+            "train": {
+                "precision": precision,
+                "recall": recall,
+                "roc_auc": roc_auc,
+            }
         }
-    }
 
-    plots = {"train": [{"tpr": i, "fpr": j} for i, j in zip(tpr, fpr)]}
+        plots = {"train": [{"tpr": i, "fpr": j} for i, j in zip(tpr, fpr)]}
 
-    with open(str(path_to_metrics_storage / "metrics.json"), "w") as handler:
-        json.dump(metrics, handler)
+        with open(str(path_to_metrics_storage / "metrics.json"), "w") as handler:
+            json.dump(metrics, handler)
 
-    with open(str(path_to_metrics_storage / "plots.json"), "w") as handler:
-        json.dump(plots, handler)
+        with open(str(path_to_metrics_storage / "plots.json"), "w") as handler:
+            json.dump(plots, handler)
 
-    logger.info("done!")
+        logger.info("done!")
 
 
 if __name__ == "__main__":
